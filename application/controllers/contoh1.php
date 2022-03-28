8924e5831b6b90d2ac3000df962fae672923995e
lailaa24 <lailatul.romadhon24@gmail,com>
2022-03-29 02:36:21 +0700

    menambakan file pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo"Nama saya Lailatul Romadhon 
+            saya tinggal di daerah tangerang
+            berenang adalah hobi saya";
+    }
+}
